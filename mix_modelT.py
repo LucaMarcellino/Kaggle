c4f2e63af005c7448828dd3c69132f0bb3c867cd
Luca Marcellino <s234262@studenti.polito.it>
2022-12-05 23:26:10 +0100

    try

--- mix_modelT.py
+++ mix_modelT.py
@@ -85,7 +85,8 @@ class Bottleneck(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10, norm_type='Batch Norm', alpha_b = 0.9, alpha_g = 0.1 ):
         super(ResNet, self).__init__()
-
+        self.alpha_b = alpha_b
+        self.alpha_g = alpha_g
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
@@ -97,8 +98,7 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, norm_type=norm_type)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, norm_type=norm_type)
         self.linear = nn.Linear(512*block.expansion, num_classes)
-        self.alpha_b = alpha_b
-        self.alpha_g = alpha_g
+
 
     def _make_layer(self, block, planes, num_blocks, stride, norm_type):
         strides = [stride] + [1]*(num_blocks-1)
