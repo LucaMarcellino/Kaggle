f33dd97ccd295e292d0edeffebab96f81cc9d72b
Luca Marcellino <s234262@studenti.polito.it>
2022-12-06 00:48:58 +0100

    Groyp

--- federeted_mix.py
+++ federeted_mix.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     train_dataset, test_dataset, user_groups = get_dataset(args)
     testloader = torch.utils.data.DataLoader(test_dataset, batch_size=args.local_bs, shuffle=False, num_workers=2, generator=g)
 
-    global_net = ResNet50(norm_type = "Batch Norm")#,alpha_b = 1, alpha_g = 0)
+    global_net = ResNet50(norm_type = "Group Norm")#,alpha_b = 1, alpha_g = 0)
     global_net.to(device)
     global_net.train()
     global_weights = global_net.state_dict()
